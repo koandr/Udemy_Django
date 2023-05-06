419b1fba9410005c017798926425859f44d10f8b
Koandr <koandr00@gmail.com>
2023-05-06 17:26:42 +0300

    table is created

---
+++ crm/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Order(models.Model):
+    order_dt = models.DateTimeField(auto_now=True)
+    order_name = models.CharField(max_length=200, verbose_name='Name')
+    order_phone = models.CharField(max_length=200, verbose_name='Tel')
+
+    def __str__(self):
+        return self.order_name
